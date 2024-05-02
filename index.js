e453178b07185caa516372228ca12b42d304a2f0
SosaJuanPedro <pedro.sosa@hasar.com>
2024-05-01 21:05:30 -0300

    First version

---
+++ index.js
@@ -0,0 +1,11 @@
+const SIZE=64;
+class UserCard extends HTMLElement {
+    constructor() {
+        super();
+        const name = this.getAttribute("name");
+        const html = `<div class="card"><img src="https://avatars.githubusercontent.com/${name}?size=${SIZE}" alt="${name}"><span>${name}</span></div>`;
+        this.insertAdjacentHTML("beforeend", html);
+    }
+}
+
+customElements.define("user-card", UserCard);
